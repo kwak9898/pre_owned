de1e155c4308a375406aca0f4b7f7e54b43dd6b1
kwaktaemin <ian.kwak@mergerity.com>
2022-11-10 17:08:45 +0900

    import members

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MembersController } from './members/members.controller';
-import { MembersService } from './members/members.service';
-import { MembersModule } from './members/members.module';
+import { MembersController } from './domains/members/members.controller';
+import { MembersService } from './domains/members/members.service';
+import { MembersModule } from './domains/members/members.module';
 
 @Module({
   imports: [MembersModule],
