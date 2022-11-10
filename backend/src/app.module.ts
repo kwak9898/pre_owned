10d155e764e9ce68f9eec2fbaf20b886610e4815
kwaktaemin <ian.kwak@mergerity.com>
2022-11-10 17:05:40 +0900

    crete members

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MembersController } from './members/members.controller';
+import { MembersService } from './members/members.service';
+import { MembersModule } from './members/members.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MembersModule],
+  controllers: [AppController, MembersController],
+  providers: [AppService, MembersService],
 })
 export class AppModule {}
