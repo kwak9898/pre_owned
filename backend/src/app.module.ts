32de989e1d823ae7874241c76960d66e6e2229b9
kwaktaemin <ian.kwak@mergerity.com>
2022-11-10 17:29:00 +0900

    database config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,23 @@ import { AppService } from './app.service';
 import { MembersController } from './domains/members/members.controller';
 import { MembersService } from './domains/members/members.service';
 import { MembersModule } from './domains/members/members.module';
+import * as Joi from '@hapi/joi';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MembersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    MembersModule,
+  ],
   controllers: [AppController, MembersController],
   providers: [AppService, MembersService],
 })
