f51ab6c9ec784593053e2b2c186409ca83e8d804
kwaktaemin <ian.kwak@mergerity.com>
2022-11-11 12:36:36 +0900

    email, password 정규 표현식 수정

--- backend/src/domains/members/dto/createMember.dto.ts
+++ backend/src/domains/members/dto/createMember.dto.ts
@@ -11,9 +11,9 @@ export class createMemberDto {
   @IsNotEmpty()
   @MinLength(5)
   @MaxLength(30)
-  // @Matches(/^[a-zA-Z0-9~@]*$/, {
-  //   message: '이메일 형식으로 5자 이상 30자 이하로 작성해주세요.',
-  // })
+  @Matches(/^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, {
+    message: '이메일 형식으로 5자 이상 30자 이하로 작성해주세요.',
+  })
   email: string;
 
   @IsString()
@@ -26,8 +26,8 @@ export class createMemberDto {
   @IsNotEmpty()
   @MinLength(4)
   @MaxLength(20)
-  @Matches(/^[a-zA-Z0-9~!@#$%^&*]*$/, {
-    message: '영문, 숫자, 특수문자의 혼합으로 설정해주세요.',
+  @Matches(/(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]/, {
+    message: '대/소문자, 숫자, 특수문자의 혼합으로 설정해주세요.',
   })
   password: string;
 
@@ -35,6 +35,6 @@ export class createMemberDto {
   @IsNotEmpty()
   @MinLength(4)
   @MaxLength(20)
-  @Matches(/^[a-zA-Z0-9~!@#$%^&*]*$/)
+  @Matches(/(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]/)
   confirmPassword: string;
 }
