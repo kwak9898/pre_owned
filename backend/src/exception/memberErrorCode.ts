c145741d608e0f97f761db88eb5f764a1c610140
kwaktaemin <ian.kwak@mergerity.com>
2022-11-11 10:37:28 +0900

    Member 생성 관련 Error Code 생성

--- backend/src/exception/memberErrorCode.ts
+++ backend/src/exception/memberErrorCode.ts
@@ -3,4 +3,8 @@ export const MEMBER_EXCEPTION = {
     code: 'MEMBER_CODE_EXIST',
     message: '이미 존재하는 이메일입니다.',
   },
+  MEMBER_CODE_PASSWORD_NOT_SAME: {
+    code: 'MEMBER_CODE_PASSWORD_NOT_SAME',
+    message: '비밀번호와 확인 비밀번호가 일치하지 않습니다.',
+  },
 };
