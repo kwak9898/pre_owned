1e8c823a9463b45e87c564bfbdbc5d6f93f2affe
kwaktaemin <ian.kwak@mergerity.com>
2022-11-18 18:24:31 +0900

    route setting

--- frontend/src/service/axios.service.ts
+++
@@ -1,8 +0,0 @@
-import axios, { AxiosInstance } from 'axios';
-
-export default class AxiosService {
-  static readonly instance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
-    timeout: 100000,
-  })
-}
