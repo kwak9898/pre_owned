233f99585bd4fb82ffdd6bc215ea33e9375d3256
kwaktaemin <ian.kwak@mergerity.com>
2022-11-18 17:59:59 +0900

    axios api server url setting

--- frontend/src/service/axios.service.ts
+++ frontend/src/service/axios.service.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 export default class AxiosService {
   static readonly instance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://localhost:3000',
     timeout: 100000,
   })
 }
