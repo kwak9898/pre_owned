9e6f8b65ecd8a827bf3c0aaf2a4d0ba9ac1df743
kwak9898 <kwak9898@gitbub.com>
2022-11-21 18:09:49 +0900

    axios 전역 설정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router";
+import axios from "axios";
 import './assets/common.css';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$axios = axios;
+app.config.globalProperties.$serverUrl = '//localhost:3000';
+app.use(router).mount('#app')
