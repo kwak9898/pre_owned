59baa289be360306ad416a28547670f25e0e248c
kwak9898 <kwak9898@gitbub.com>
2022-11-21 18:23:58 +0900

    게시물 전체 조회 public으로 변경 및 cors 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger/api', app, document);
 
+  app.enableCors();
   await app.listen(3000);
 }
 
