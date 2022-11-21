7cc1e4a898f37f4a43dc1d1d104cf997c5867c6d
kwak9898 <kwak9898@gitbub.com>
2022-11-21 18:37:14 +0900

    Network 통신 문제 해결 중

--- frontend/src/views/production/ProductList.vue
+++ frontend/src/views/production/ProductList.vue
@@ -1,5 +1,5 @@
 <template>
-  <diV class="product-list">
+  <div class="board-list">
     <div class="common-buttons">
       <button type="button" class="w3-button w3-round w3-blue-gray" v-on:click="fnWrite">등록</button>
     </div>
@@ -31,32 +31,32 @@
     </table>
     <div class="pagination w3-bar w3-padding-16 w3-small" v-if="paging.total_list_cnt > 0">
       <span class="pg">
-      <a href="javascript:;" @click="fnPage(1)" class="first w3-button w3-border">&lt;&lt;</a>
+      <a href="javascript:;" @click="fnPage(1)" class="first w3-button w3-bar-item w3-border">&lt;&lt;</a>
       <a href="javascript:;" v-if="paging.start_page > 10" @click="fnPage(`${paging.start_page-1}`)"
-         class="prev w3-button w3-border">&lt;</a>
+         class="prev w3-button w3-bar-item w3-border">&lt;</a>
       <template v-for=" (n,index) in paginavigation()">
-          <template v-if="paging.page==n">
-              <strong class="w3-button w3-border w3-green" :key="index">{{ n }}</strong>
+          <template v-if="paging.page===n">
+              <strong class="w3-button w3-bar-item w3-border w3-green" :key="index">{{ n }}</strong>
           </template>
           <template v-else>
-              <a class="w3-button w3-border" href="javascript:;" @click="fnPage(`${n}`)" :key="index">{{ n }}</a>
+              <a class="w3-button w3-bar-item w3-border" href="javascript:;" @click="fnPage(`${n}`)" :key="index">{{ n }}</a>
           </template>
       </template>
       <a href="javascript:;" v-if="paging.total_page_cnt > paging.end_page"
-         @click="fnPage(`${paging.end_page+1}`)" class="next w3-button w3-border">&gt;</a>
-      <a href="javascript:;" @click="fnPage(`${paging.total_page_cnt}`)" class="last w3-button w3-border">&gt;&gt;</a>
+         @click="fnPage(`${paging.end_page+1}`)" class="next w3-button w3-bar-item w3-border">&gt;</a>
+      <a href="javascript:;" @click="fnPage(`${paging.total_page_cnt}`)" class="last w3-button w3-bar-item w3-border">&gt;&gt;</a>
       </span>
     </div>
-  </diV>
+  </div>
 </template>
 
 <script>
 export default {
-  data() {
+  data() { //변수생성
     return {
-      requestBody: {},
-      list: {},
-      no: '',
+      requestBody: {}, //리스트 페이지 데이터전송
+      list: {}, //리스트 데이터
+      no: '', //게시판 숫자처리
       paging: {
         block: 0,
         end_page: 0,
@@ -65,15 +65,16 @@ export default {
         page_size: 0,
         prev_block: 0,
         start_index: 0,
+        start_page: 0,
         total_block_cnt: 0,
         total_list_cnt: 0,
-        total_page_cht: 0,
-      },
+        total_page_cnt: 0,
+      }, //페이징 데이터
       page: this.$route.query.page ? this.$route.query.page : 1,
       size: this.$route.query.size ? this.$route.query.size : 10,
       keyword: this.$route.query.keyword,
-      paginavigation: function () {
-        let pageNumber = [];
+      paginavigation: function () { //페이징 처리 for문 커스텀
+        let pageNumber = [] //;
         let start_page = this.paging.start_page;
         let end_page = this.paging.end_page;
         for (let i = start_page; i <= end_page; i++) pageNumber.push(i);
@@ -82,42 +83,28 @@ export default {
     }
   },
   mounted() {
-    this.fnGetList();
+    this.fnGetList()
   },
   methods: {
     fnGetList() {
-      this.list = [
-        {
-          "id": 1,
-          "title": "title",
-          "name": "name",
-          "itemPrice": "item",
-          "content": "good",
-          "area": "seoul city",
-          "type": "pro",
-          "createdAt": "2022-11-21",
-        },
-        {
-          "id": 1,
-          "title": "title",
-          "name": "name",
-          "itemPrice": "item",
-          "content": "good",
-          "area": "seoul city",
-          "type": "pro",
-          "createdAt": "2022-11-21",
-        },
-        {
-          "id": 1,
-          "title": "title",
-          "name": "name",
-          "itemPrice": "item",
-          "content": "good",
-          "area": "seoul city",
-          "type": "pro",
-          "createdAt": "2022-11-21",
+      this.requestBody = { // 데이터 전송
+        keyword: this.keyword,
+        page: this.page,
+        size: this.size
+      }
+
+      this.$axios.get(this.$serverUrl + "/items", {
+        params: this.requestBody,
+        headers: {}
+      }).then((res) => {
+
+        this.list = res.data  //서버에서 데이터를 목록으로 보내므로 바로 할당하여 사용할 수 있다.
+
+      }).catch((err) => {
+        if (err.message.indexOf('Network Error') > -1) {
+          alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
         }
-      ]
+      })
     }
   }
 }
