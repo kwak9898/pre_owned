9de5c7b4fd725b16e12a5dd5cc012216d3340401
kwak9898 <kwak9898@gitbub.com>
2022-11-21 16:49:15 +0900

    layout

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/board/list">게시판</router-link>
+      <router-link to="/board/list">board</router-link>
     </div>
   </header>
   <hr/>
