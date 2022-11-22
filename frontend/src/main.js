c9beb447c915473f4b444d01a446c3e3334984ad
kwak9898 <kwak9898@gitbub.com>
2022-11-22 18:40:33 +0900

    왜 함수를 못 불러오지

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from "@/router";
 import axios from "axios";
 import './assets/common.css';
+import mixins from "./mixins";
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios;
 app.config.globalProperties.$serverUrl = '//localhost:3000';
-app.use(router).mount('#app')
+app.use(router)
+app.use(mixins)
+app.mount('#app')
--- frontend/src/views/production/ProductList.vue
+++ frontend/src/views/production/ProductList.vue
@@ -1,89 +1,74 @@
 <template>
-  <div class="board-list">
-    <div class="common-buttons">
-      <button type="button" class="w3-button w3-round w3-blue-gray" v-on:click="fnWrite">등록</button>
+  <main class="mt-5">
+    <div class="container">
+      <div v-if="itemList == 0" class="row">
+        <h5 class="m-4">등록된 상품이 없습니다.</h5>
+      </div>
+      <div v-else class="row">
+        <div
+            class="col-xl-3 col-lg-4 col-md-6"
+            :key="i"
+            v-for="(item, i) in itemList"
+        >
+          <div class="card text-start" style="width: 18rem; border: 0px">
+<!--            <a @click="goToDetail(item.id)" style="cursor: pointer"-->
+<!--            ><img-->
+<!--                :src="`/download/${item.id}/${item.path}`"-->
+<!--                class="card-img-top"-->
+<!--                alt="..."-->
+<!--            /></a>-->
+            <div class="card-body">
+              <h5 class="card-title">{{ item.item_Name }}</h5>
+              <p class="card-text">
+                {{ getCurrencyFormat(item.item_price) }}원
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <table class="w3-table-all">
-      <thead>
-      <tr>
-        <th>No.</th>
-        <th>title</th>
-        <th>name</th>
-        <th>price</th>
-        <th>content</th>
-        <th>area</th>
-        <th>type</th>
-        <th>created</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="(item, i) in list" :key="i">
-        <td>{{ item.itemId }}</td>
-        <td><a v-on:click="fnView(`${item.itemId}`)">{{item.title}}</a> </td>
-        <td>{{item.itemName}}</td>
-        <td>{{ item.item }}</td>
-        <td>{{ item.itemPrice }}</td>
-        <td>{{item.itemContent}}</td>
-        <td>{{ item.area }}</td>
-        <td>{{ item.type }}</td>
-      </tr>
-      </tbody>
-    </table>
-<!--    <div class="pagination w3-bar w3-padding-16 w3-small" v-if="paging.total_list_cnt > 0">-->
-<!--      <span class="pg">-->
-<!--      <a href="javascript:;" @click="fnPage(1)" class="first w3-button w3-bar-item w3-border">&lt;&lt;</a>-->
-<!--      <a href="javascript:;" v-if="paging.start_page > 10" @click="fnPage(`${paging.start_page-1}`)"-->
-<!--         class="prev w3-button w3-bar-item w3-border">&lt;</a>-->
-<!--      <template v-for=" (n,index) in pagination()">-->
-<!--          <template v-if="paging.page===n">-->
-<!--              <strong class="w3-button w3-bar-item w3-border w3-green" :key="index">{{ n }}</strong>-->
-<!--          </template>-->
-<!--          <template v-else>-->
-<!--              <a class="w3-button w3-bar-item w3-border" href="javascript:;" @click="fnPage(`${n}`)" :key="index">{{ n }}</a>-->
-<!--          </template>-->
-<!--      </template>-->
-<!--      <a href="javascript:;" v-if="paging.total_page_cnt > paging.end_page"-->
-<!--         @click="fnPage(`${paging.end_page+1}`)" class="next w3-button w3-bar-item w3-border">&gt;</a>-->
-<!--      <a href="javascript:;" @click="fnPage(`${paging.total_page_cnt}`)" class="last w3-button w3-bar-item w3-border">&gt;&gt;</a>-->
-<!--      </span>-->
-<!--    </div>-->
-  </div>
+  </main>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      requestBody: {},
-      list: [],
-      no: '',
-      pageNum: 1,
-    }
-  },
-  mounted() {
-    this.fnGetList()
-  },
-  methods: {
-    fnGetList(pageNum) {
-      this.requestBody = { // 데이터 전송
-        keyword: this.keyword,
-        page: this.page,
-        size: this.size
+      itemList: [],
+      item_Name: '',
+      item_price: '',
+      pageNumber: 1
       }
+    },
+    // created() {
+    // //   this.getProductList(); // created 단계에서 getProductList를 실행시켜 data 미리 호출
+    // },
+    mounted() {
+    this.getList();
+    },
+  methods: {
+    async getCurrencyFormat(value) {
+      return this.$currencyFormat(value)
+    },
 
+    async getList() {
       this.$axios
-          .get(this.$serverUrl + "/items?limit=", + pageNum)
+          .get(this.$serverUrl + "/items", {})
           .then((res) => {
-            const a = this.list = res.data
-            console.log("ㅁ : ", a)
-          })
-          .catch((err) => {
-            console.log("error: ", err)
-            if (err.message.indexOf('Network Error') > -1) {
-              alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
-            }
+            this.itemList = res.data;
+            console.log("items : ", this.itemList);
+          }).catch((error) => {
+            console.log(error);
+        if (error.message.indexOf('Network Error') > -1) {
+          alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.');
+        }
       })
-    }
+    },
+
+    goToDetail(product_id) {
+      // 제품 이미지 클릭시 product_id를 받아 제품 상세페이지로 router되도록 설정
+      this.$router.push({ path: "/detail", query: { product_id: product_id } }); // path중 /detail이 들어가면 product_id를 파라미터로 받아 라우터 시킴
+    },
+    },
   }
-}
 </script>
