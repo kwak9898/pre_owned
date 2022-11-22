dcdb3ad738ee5bdfd8c07338de679267846d244d
kwak9898 <kwak9898@gitbub.com>
2022-11-22 15:51:40 +0900

    처음부터 다시,,

--- frontend/src/views/production/ProductList.vue
+++ frontend/src/views/production/ProductList.vue
@@ -17,93 +17,71 @@
       </tr>
       </thead>
       <tbody>
-      <tr v-for="(row, id) in list" :key="id">
-        <td>{{ row.id }}</td>
-        <td><a v-on:click="fnView(`${row.idx}`)">{{row.title}}</a> </td>
-        <td>{{ row.name }}</td>
-        <td>{{ row.itemPrice }}</td>
-        <td>{{ row.content }}</td>
-        <td>{{ row.area }}</td>
-        <td>{{ row.type }}</td>
-        <td>{{ row.createdAt }}</td>
+      <tr v-for="(item, i) in list" :key="i">
+        <td>{{ item.itemId }}</td>
+        <td><a v-on:click="fnView(`${item.itemId}`)">{{item.title}}</a> </td>
+        <td>{{item.itemName}}</td>
+        <td>{{ item.item }}</td>
+        <td>{{ item.itemPrice }}</td>
+        <td>{{item.itemContent}}</td>
+        <td>{{ item.area }}</td>
+        <td>{{ item.type }}</td>
       </tr>
       </tbody>
     </table>
-    <div class="pagination w3-bar w3-padding-16 w3-small" v-if="paging.total_list_cnt > 0">
-      <span class="pg">
-      <a href="javascript:;" @click="fnPage(1)" class="first w3-button w3-bar-item w3-border">&lt;&lt;</a>
-      <a href="javascript:;" v-if="paging.start_page > 10" @click="fnPage(`${paging.start_page-1}`)"
-         class="prev w3-button w3-bar-item w3-border">&lt;</a>
-      <template v-for=" (n,index) in paginavigation()">
-          <template v-if="paging.page===n">
-              <strong class="w3-button w3-bar-item w3-border w3-green" :key="index">{{ n }}</strong>
-          </template>
-          <template v-else>
-              <a class="w3-button w3-bar-item w3-border" href="javascript:;" @click="fnPage(`${n}`)" :key="index">{{ n }}</a>
-          </template>
-      </template>
-      <a href="javascript:;" v-if="paging.total_page_cnt > paging.end_page"
-         @click="fnPage(`${paging.end_page+1}`)" class="next w3-button w3-bar-item w3-border">&gt;</a>
-      <a href="javascript:;" @click="fnPage(`${paging.total_page_cnt}`)" class="last w3-button w3-bar-item w3-border">&gt;&gt;</a>
-      </span>
-    </div>
+<!--    <div class="pagination w3-bar w3-padding-16 w3-small" v-if="paging.total_list_cnt > 0">-->
+<!--      <span class="pg">-->
+<!--      <a href="javascript:;" @click="fnPage(1)" class="first w3-button w3-bar-item w3-border">&lt;&lt;</a>-->
+<!--      <a href="javascript:;" v-if="paging.start_page > 10" @click="fnPage(`${paging.start_page-1}`)"-->
+<!--         class="prev w3-button w3-bar-item w3-border">&lt;</a>-->
+<!--      <template v-for=" (n,index) in pagination()">-->
+<!--          <template v-if="paging.page===n">-->
+<!--              <strong class="w3-button w3-bar-item w3-border w3-green" :key="index">{{ n }}</strong>-->
+<!--          </template>-->
+<!--          <template v-else>-->
+<!--              <a class="w3-button w3-bar-item w3-border" href="javascript:;" @click="fnPage(`${n}`)" :key="index">{{ n }}</a>-->
+<!--          </template>-->
+<!--      </template>-->
+<!--      <a href="javascript:;" v-if="paging.total_page_cnt > paging.end_page"-->
+<!--         @click="fnPage(`${paging.end_page+1}`)" class="next w3-button w3-bar-item w3-border">&gt;</a>-->
+<!--      <a href="javascript:;" @click="fnPage(`${paging.total_page_cnt}`)" class="last w3-button w3-bar-item w3-border">&gt;&gt;</a>-->
+<!--      </span>-->
+<!--    </div>-->
   </div>
 </template>
 
 <script>
 export default {
-  data() { //변수생성
+  data() {
     return {
-      requestBody: {}, //리스트 페이지 데이터전송
-      list: {}, //리스트 데이터
-      no: '', //게시판 숫자처리
-      paging: {
-        block: 0,
-        end_page: 0,
-        next_block: 0,
-        page: 0,
-        page_size: 0,
-        prev_block: 0,
-        start_index: 0,
-        start_page: 0,
-        total_block_cnt: 0,
-        total_list_cnt: 0,
-        total_page_cnt: 0,
-      }, //페이징 데이터
-      page: this.$route.query.page ? this.$route.query.page : 1,
-      size: this.$route.query.size ? this.$route.query.size : 10,
-      keyword: this.$route.query.keyword,
-      paginavigation: function () { //페이징 처리 for문 커스텀
-        let pageNumber = [] //;
-        let start_page = this.paging.start_page;
-        let end_page = this.paging.end_page;
-        for (let i = start_page; i <= end_page; i++) pageNumber.push(i);
-        return pageNumber;
-      }
+      requestBody: {},
+      list: [],
+      no: '',
+      pageNum: 1,
     }
   },
   mounted() {
     this.fnGetList()
   },
   methods: {
-    fnGetList() {
+    fnGetList(pageNum) {
       this.requestBody = { // 데이터 전송
         keyword: this.keyword,
         page: this.page,
         size: this.size
       }
 
-      this.$axios.get(this.$serverUrl + "/items", {
-        params: this.requestBody,
-        headers: {}
-      }).then((res) => {
-
-        this.list = res.data  //서버에서 데이터를 목록으로 보내므로 바로 할당하여 사용할 수 있다.
-
-      }).catch((err) => {
-        if (err.message.indexOf('Network Error') > -1) {
-          alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
-        }
+      this.$axios
+          .get(this.$serverUrl + "/items?limit=", + pageNum)
+          .then((res) => {
+            const a = this.list = res.data
+            console.log("ㅁ : ", a)
+          })
+          .catch((err) => {
+            console.log("error: ", err)
+            if (err.message.indexOf('Network Error') > -1) {
+              alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
+            }
       })
     }
   }
