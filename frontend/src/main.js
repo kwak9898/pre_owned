6253cdb52e77292d0183d2ae97001365da3cecc6
kwak9898 <kwak9898@gitbub.com>
2022-11-23 10:40:45 +0900

    기존 Axios Settings 제거

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router";
-import axios from "axios";
 import './assets/common.css';
 import mixins from "./mixins";
 
 const app = createApp(App)
-app.config.globalProperties.$axios = axios;
-app.config.globalProperties.$serverUrl = '//localhost:3000';
+// app.config.globalProperties.$axios = axios;
+// app.config.globalProperties.$serverUrl = '//localhost:3000';
 app.use(router)
 app.use(mixins)
 app.mount('#app')
